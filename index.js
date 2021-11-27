44d83cff65ce28b85c6bf82c91fa4d67fb05d59c
Samantha Fisher <sfisher@Samanthas-MacBook-Air.local>
2021-11-27 09:20:11 +0000

    green square moves around with mouse and no longer leaves a trail

--- index.js
+++ index.js
@@ -3,36 +3,29 @@ var pointerX=-1;
 var pointerY=-1;
 var running = false;
 var canvas;
+var crect;
 var ctx;
 
-function pressed(){
-  if (!running){
-    canvas = document.getElementById('canvas');
-    ctx = canvas.getContext('2d');
-    running = true;
-  }
-  alert("doing something");
+window.onload = function init(){
+  canvas = document.getElementById('canvas');
+  ctx = canvas.getContext('2d');
+  crect = canvas.getBoundingClientRect();
 
-
-
-
-}
-
-function x1change(val){
-  pressed();
 }
 
 document.onmousemove = function(event){
-  pointerX = event.pageX;
-  pointerY = event.pageY;
+  pointerX = event.pageX - crect.left;
+  pointerY = event.pageY - crect.top;
 }
-setInterval(pointerCheck, 50);
-function pointerCheck(){
-  console.log("Cursor at" + pointerX +" "+ pointerY);
-  if (running) {
-    ctx.beginPath();
-    ctx.rect(pointerX, pointerY, 10,10);
-    ctx.fillStyle = 'green';
-    ctx.fill();
-  }
+setInterval(mainLoop, 20);
+function mainLoop(){
+  ctx.beginPath();
+  ctx.rect(0,0,400,400);
+  ctx.fillStyle = 'white';
+  ctx.fill();
+  ctx.beginPath();
+  ctx.rect(pointerX, pointerY, 10,10);
+  ctx.fillStyle = 'green';
+  ctx.fill();
+
 }
